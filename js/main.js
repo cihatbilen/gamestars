f8295643cbd8bcc4da05498b4e13f5823adea393
cihatbilen <94382897+cihatbilen@users.noreply.github.com>
2021-12-13 16:06:06 +0100

    hover merch

---
+++ js/main.js
@@ -0,0 +1,67 @@
+function showPrice(){
+  document.getElementById("item-1-text").style.display = "block";
+}
+document.getElementById("item-1").addEventListener('mouseover', showPrice);
+
+function hidePrice(){
+
+    document.getElementById("item-1-text").style.display = "none";
+}
+
+document.getElementById('item-1').addEventListener('mouseout', hidePrice);
+
+
+
+
+
+
+
+function showPrice(){
+  document.getElementById("item-2-text").style.display = "block";
+}
+document.getElementById("item-2").addEventListener('mouseover', showPrice);
+
+function hidePrice(){
+
+    document.getElementById("item-2-text").style.display = "none";
+}
+
+document.getElementById('item-2').addEventListener('mouseout', hidePrice);
+
+
+
+
+
+
+
+
+function showPrice(){
+  document.getElementById("item-3-text").style.display = "block";
+}
+document.getElementById("item-3").addEventListener('mouseover', showPrice);
+
+function hidePrice(){
+
+    document.getElementById("item-3-text").style.display = "none";
+}
+
+document.getElementById('item-3').addEventListener('mouseout', hidePrice);
+
+
+
+
+
+
+
+
+function showPrice(){
+  document.getElementById("item-4-text").style.display = "block";
+}
+document.getElementById("item-4").addEventListener('mouseover', showPrice);
+
+function hidePrice(){
+
+    document.getElementById("item-4-text").style.display = "none";
+}
+
+document.getElementById('item-4').addEventListener('mouseout', hidePrice);

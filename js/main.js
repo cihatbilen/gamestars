d3428eb29fbb6ddd71640de49c5565f6ff779c0a
cihatbilen <94382897+cihatbilen@users.noreply.github.com>
2022-01-21 12:22:52 +0100

    hamburger menu en responsive

--- js/main.js
+++ js/main.js
@@ -25,69 +25,11 @@ document.getElementById("item-4").addEventListener('mouseover', function(event){
 document.getElementById('item-4').addEventListener('mouseout', function(event){hidePrices(event.target.id);});
 
 
-//
-// function showPrice1(){
-//   document.getElementById("item-1-text").style.display = "block";
-// }
-//
-// document.getElementById("item-1").addEventListener('mouseover', function(event){showPrice1; console.log(event.target.id)}, false);
-//
-// function hidePrice1(){
-//
-//     document.getElementById("item-1-text").style.display = "none";
-// }
-//
-// document.getElementById('item-1').addEventListener('mouseout', hidePrice1);
-//
-//
-//
-//
-//
-//
-// function showPrice2(){
-//   document.getElementById("item-2-text").style.display = "block";
-// }
-// document.getElementById("item-2").addEventListener('mouseover', showPrice2);
-//
-// function hidePrice2(){
-//
-//     document.getElementById("item-2-text").style.display = "none";
-// }
-//
-// document.getElementById('item-2').addEventListener('mouseout', hidePrice2);
-//
-//
-//
-//
-//
-//
-//
-// function showPrice3(){
-//   document.getElementById("item-3-text").style.display = "block";
-// }
-// document.getElementById("item-3").addEventListener('mouseover', showPrice3);
-//
-// function hidePrice3(){
-//
-//     document.getElementById("item-3-text").style.display = "none";
-// }
-//
-// document.getElementById('item-3').addEventListener('mouseout', hidePrice3);
-//
-//
-//
-//
-//
-//
-//
-// function showPrice4(){
-//   document.getElementById("item-4-text").style.display = "block";
-// }
-// document.getElementById("item-4").addEventListener('mouseover', showPrice4);
-//
-// function hidePrice4(){
-//
-//     document.getElementById("item-4-text").style.display = "none";
-// }
-//
-// document.getElementById('item-4').addEventListener('mouseout', hidePrice4);
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
